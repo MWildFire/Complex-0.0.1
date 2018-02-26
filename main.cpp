b06632b4e2dd280cb301787d21657478575ef128
justcppdev <justcppdev@ya.ru>
2018-02-26 14:59:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Сурков Дмитрий Валерьевич. All rights reserved.
 //
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -26,83 +27,98 @@ struct complex_t
         result.real = real + other.real;
         result.imag = imag + other.imag;
         return result;
-    };
+    }
+    
     complex_t sub( complex_t other ) const
     {
         complex_t result;
         result.real = real - other.real;
         result.imag = imag - other.imag;
         return result;
-    };
+    }
+    
     complex_t mult( complex_t other ) const
     {
         complex_t result;
         result.real = real * other.real - imag * other.imag;
         result.imag = real * other.imag - imag * other.real;
         return result;
-    };
+    }
+    
     complex_t div( complex_t other ) const
     {
         complex_t result;
         result.real = (real * other.real + imag * other.imag)/(other.real * other.real - other.imag * other.imag);
         result.imag = (imag * other.real - real * other.imag)/(other.real * other.real - other.imag * other.imag);
         return result;
-    };
+    }
     
     istream & read(istream & stream)
     {
         char op;
         return stream >> op >> real >> op >> imag >> op;
-    };
-    ostream & write(ostream & stream )
+    }
+    
+    ostream & write(ostream & stream ) const
     {
         return stream << "(" << real << "," << imag << ")";
-    };
+    }
 };
 
-int main ()
+int main()
 {
-    complex_t x, y, result;
+    complex_t x, y;
     char op;
-    if (x.read(cin) && (cin >> op) && y.read(cin));
+    
+    // std::cin (1, 2) + (3, 4)\n
+    // stream
+    
+    std::string line;
+    std::getline( std::cin, line );
+    // std::cin
+    // line "(1, 2) + (3, 4)"
+    // stream
+    std::istringstream stream( line );
+    // std::cin
+    // line "(1, 2) + (3, 4)"
+    // stream (1, 2) + (3, 4)
+    
+    if (x.read(stream) && (stream >> op) && y.read(stream)) 
     {
+        complex_t result;
         switch (op)
         {
-                case '+':
+            case '+':
             {
                 result = x.add(y);
                 result.write(cout);
                 break;
             }
-                case '-':
+            case '-':
             {
                 result = x.sub(y);
                 result.write(cout);
                 break;
             }
-                case '*':
+            case '*':
             {
                 result = x.mult(y);
                 result.write(cout);
                 break;
             }
-                case '/':
+            case '/':
             {
                 result = x.div(y);
                 result.write(cout);
                 break;
             }
-            default:
+            default: 
+            {
                 cout << "An error has occured while reading input data" << endl;
                 break;
+            }
         }
     }
-    system ("pause");
+
+    std::cin.get();
 }
-
-
-    
-
-
-
-
